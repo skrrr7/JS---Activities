ba1298081df218deb21db13f2189acc781d6a375
Raphael Jay Escosia <escosiaraphael70@gmail.com>
2025-01-23 16:07:00 +0800

    Added solution for s03 activity

--- s03/discussion/index.js
+++ s03/discussion/index.js
@@ -41,27 +41,3 @@ let address = "Talisay City";
 console.log("Address:", address);
 
 
-let realName = "Steve Rogers";  
-console.log("My real name is:");
-console.log(realName);
-
-let currentAge = 21;  
-console.log("My current age is: ");
-console.log(currentAge);
-
-let interestRate = 30;  
-console.log("The current interest rate: ");
-console.log(interestRate);
-
-let x = "EcommerceAPI";
-console.log("The current login key is: ");
-console.log(x);
-
-let bankCode = "BANK123";  
-console.log("Bank code: ");
-console.log(bankCode);
-
-let bankBalance = 500;  
-console.log("Current bank balance: ");
-console.log(bankBalance);
-
