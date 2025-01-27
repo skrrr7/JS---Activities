f9e9457039c1b2e5b795a69cbeaafa387684d5c3
Raphael Jay Escosia <escosiaraphael70@gmail.com>
2025-01-27 13:50:46 +0800

    s05 discussion

---
+++ s05/discussion/index.js
@@ -0,0 +1,57 @@
+console.log("hello world!");
+
+// arithmethic operators
+
+let x = 1397;
+let y = 7831;
+
+let sum = x + y;
+console.log("Result of addition operator: " + sum);
+
+let difference = x - y;
+console.log("Result of subtraction operator: " + difference);
+
+let product = x * y;
+console.log("Result of multiplication operator: " + product);
+
+let quotient = x/y;
+console.log("Result of division operator: " + quotient);
+
+
+// assignment operator(=)
+
+let assignmentNumber = 8;
+
+
+// addition assignment operator(+)
+
+assignmentNumber = assignmentNumber + 2;
+console.log("Result of addition: " + assignmentNumber);
+
+// shorthand for assignmentNumber 
+assignmentNumber += 2;
+console.log("Result of addition: " + assignmentNumber);
+
+// -=, *=, /=
+assignmentNumber -= 2;
+console.log("Result of subtraction: " + assignmentNumber);
+
+// increment and decrement
+let z = 1;
+
+let increment = ++z;
+
+console.log("Result pre inc: " + increment);
+console.log("Result: " + z);
+
+incrememnt = z++;
+console.log("Result post: " + increment);
+console.log("Result: " + z);
+
+let decrement = --z;
+console.log("Result pre dec: " + decrement);
+console.log("Result: " + z);
+
+decrement = z--;
+console.log("Result post dec: " + decrement);
+console.log("Result: " + z);
