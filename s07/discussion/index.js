61babe595809913e826d49c55047a88f2be18689
Raphael Jay Escosia <escosiaraphael70@gmail.com>
2025-01-30 16:02:36 +0800

    s07 discussion

---
+++ s07/discussion/index.js
@@ -0,0 +1,43 @@
+//console.log("Hello world");
+
+// FUNCTIONS
+
+function sayHello(){
+    console.log("Hello world");
+
+}
+
+function displayCarInfo(){
+    console.log("Brand: toyota");
+    console.log("Type: Sedan");
+    console.log("Price: 1,500,000");
+}
+
+// function calling
+sayHello();
+displayCarInfo();
+
+function displayCourse(){
+    let course = ["Sci 101", "Math 101", "Eng 101"];
+    console.log(course);
+}
+displayCourse();
+
+function getFlower(){
+    let name = "sampaguita";
+    console.log(name);
+}
+getFlower();
+
+// foo, bar
+
+function foo(){
+    console.log(25%5);
+}
+
+foo();
+
+function displayModulo(){
+    console.log(50%5);
+}
+displayModulo();
