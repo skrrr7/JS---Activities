3386fdac6eb7904cc72c8c22a25ebfd5935d2985
Raphael Jay Escosia <escosiaraphael70@gmail.com>
2025-02-13 13:53:58 +0800

    Added solution for the s13 activity

--- s13/activity/index.js
+++ s13/activity/index.js
@@ -21,7 +21,7 @@ function printOddNumbers(num) {
 
     for (let i = 1; i <= num; i++) {
         if (i % 2 === 0) {
-            continue; 
+             continue; 
         }
 
         console.log("Continue and Break:", i); 
