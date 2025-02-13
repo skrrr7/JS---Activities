ff1189b084b913562c5a29704c8824e49bb7ce68
Raphael Jay Escosia <escosiaraphael70@gmail.com>
2025-02-13 23:17:02 +0800

    s14 discussion

---
+++ s14/discussion/index.js
@@ -0,0 +1,16 @@
+// for loop
+
+for(let count = 0; count <= 20; count++){
+    console.log("For loop: " + count);
+}
+
+let mystring = "alex";
+
+console.log(mystring.length);
+
+console.log(mystring[0]);
+
+
+for(let x = 0; x < mystring.length; x++){
+    console.log("Individual letters: " + mystring[x]);
+}
