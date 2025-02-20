93dbf584bdf13bf8ea2b72e391af4a372ce7cd17
Raphael Jay Escosia <escosiaraphael70@gmail.com>
2025-02-20 15:16:09 +0800

    s18 discussion

---
+++ s18/discussion/index.js
@@ -0,0 +1,11 @@
+// array mutators fill , sort , copywithin, reverse
+
+
+let guestList = ["Charlie", "Alice", "David", "Bob", "Eve"];
+console.log(guestList);
+
+guestList.sort();
+console.log(guestList);
+
+guestList.reverse();
+console.log(guestList);
